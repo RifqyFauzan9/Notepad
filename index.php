e3b1a3f2beee2529a6271158144ffcb7d18b9a84
Rifqy Fauzan <r1fqyf4uz4n@gmail.com>
2023-11-21 08:57:59 +0700

    aa

--- index.php
+++ index.php
@@ -15,20 +15,64 @@ $no = 0;
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>NotepadKu</title>
-    <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" href="img/notepad2.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+          * {
+        font-family: monospace;
+    }
+
+    body {
+      background: url(img/leaves.webp);
+    }
+
+    .none {
+        text-decoration: none;
+        color: black;
+    }
+
+    nav h3 img {
+        width: 70px;
+        margin-bottom: 5px;
+    }
+
+    .footer {
+      background: blue; 
+      color: white; 
+      display:flex; 
+      justify-content:left; 
+      margin-top: -1rem; 
+      margin-bottom:1rem; 
+      max-width:fit-content; 
+      border-radius:1rem; 
+      padding: 10px;
+    }
+
+    .navigasi {
+      border: 3px solid lightblue; 
+      max-width: 300px;
+    }
+
+    .kartu {
+      max-width: fit-content; border: solid 3px lightblue; display: inline-block;
+    }
+
+    .card-link {
+      text-decoration: none; 
+      color: blue;
+    }
+    </style>
   </head>
-  <body style="background: url(img/leaves.webp);">
-    <div class="container contentt">
+  <body>
+    <div class="container">
             <nav><a class="none" href="index.php"><h3 class="mt-3"><img src="img/notepad2.png">NotepadKu</h3></a></nav>
-  <div class="footer" style="background: blue; color: white; display:flex; justify-content:left; margin-top: -1rem; margin-bottom:1rem; max-width:fit-content; border-radius:1rem; padding: 10px;">Copyright © 2023 by Rifqy Fauzan</div>
+  <div class="footer">Copyright © 2023 by Rifqy Fauzan</div>
     
-    <div class="card shadow bg-body-tertiary rounded mb-3 navigasi" style="border: 3px solid aquamarine;">
+    <div class="card shadow bg-body-tertiary rounded mb-3 navigasi">
         <div class="card-body">
             <form action="insert.php" method="post" >
-            <input class="form-control me-2" name="title_note" placeholder="Title">
-            <input class="form-control me-2 mt-2" name="note_note" placeholder="Text">
+            <input required class="form-control me-2" name="title_note" placeholder="Title">
+            <input required class="form-control me-2 mt-2" name="note_note" placeholder="Text">
             <button class="btn btn-primary mt-2 kanan" type="submit">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-upload kanan" viewBox="0 0 16 16">
   <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
@@ -42,12 +86,12 @@ $no = 0;
     while($result = mysqli_fetch_assoc($sql)) {
       
     ?>
-    <div class="card mb-2" style="max-width: fit-content; border: solid 3px aquamarine; display: inline-block;">
+    <div class="card mb-2 kartu">
         <div class="card-body">
           <h5 class="card-title"><?php echo ++$no?>. <?php echo $result['title_note'];?></h5>
           <p class="card-text"><?php echo $result['note_note'];?></p>
-          <a href="update.php?edit=<?php echo $result['id_note'];?>" class="card-link" style="text-decoration: none; color: blue;">Edit</a>
-          <a href="delete.php?delete=<?php echo $result['id_note'];?>" class="card-link" style="text-decoration: none; color: blue;">Delete</a>
+          <a href="update.php?edit=<?php echo $result['id_note'];?>" class="card-link">Edit</a>
+          <a href="delete.php?delete=<?php echo $result['id_note'];?>" class="card-link">Delete</a>
         </div>
       </div>
       <?php
--- update.php
+++ update.php
@@ -14,19 +14,64 @@ $no = 0;
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>NotepadKu</title>
-    <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" href="img/notepad2.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+      * {
+        font-family: monospace;
+    }
+
+    body {
+      background: url(img/leaves.webp);
+    }
+
+    .none {
+        text-decoration: none;
+        color: black;
+    }
+
+    nav h3 img {
+        width: 70px;
+        margin-bottom: 5px;
+    }
+
+    .footer {
+      background: blue; 
+      color: white; 
+      display:flex; 
+      justify-content:left; 
+      margin-top: -1rem; 
+      margin-bottom:1rem; 
+      max-width:fit-content; 
+      border-radius:1rem; 
+      padding: 10px;
+    }
+
+    .navigasi {
+      border: 3px solid lightblue; 
+      max-width: 300px;
+    }
+
+    .kartu {
+      max-width: fit-content; border: solid 3px lightblue; display: inline-block;
+    }
+
+    .card-link {
+      text-decoration: none; 
+      color: blue;
+    }
+    </style>
   </head>
-  <body style="background: url(img/leaves.webp);">
-    <div class="container contentt">
-            <nav><a class="none" href="index.php"><h3 class="mt-3"><img src="img/notepad2.png">Edit</h3></a></nav>
-            <div class="footer" style="background: blue; color: white; display:flex; justify-content:left; margin-top: -1rem; margin-bottom:1rem; max-width:fit-content; border-radius:1rem; padding: 10px;">Copyright © 2023 by Rifqy Fauzan</div>
-    <div class="card shadow bg-body-tertiary rounded mb-3 navigasi" style="border: 3px solid aquamarine;">
+  <body>
+    <div class="container">
+            <nav><a class="none" href="index.php"><h3 class="mt-3 none"><img src="img/notepad2.png">Edit</h3></a></nav>
+            <div class="footer">Copyright © 2023 by Rifqy Fauzan</div>
+    <div class="card shadow bg-body-tertiary rounded mb-3 navigasi">
         <div class="card-body">
-            <form action="insert.php" method="post" >
-            <input class="form-control me-2" name="title_note" placeholder="Title" value="<?php echo $result ['title_note'] ?>">
-            <input class="form-control me-2 mt-2" name="note_note" placeholder="Text" value="<?php echo $result ['note_note'] ?>">
+            <form action="update_dua.php" method="post" >
+            <input class="form-control me-2" name="title_note" placeholder="Title" value="<?php echo $result['title_note']?>">
+            <input class="form-control me-2 mt-2" name="note_note" placeholder="Text" value="<?php echo $result['note_note']?>">
+            <input type="hidden" name="id" value="<?php echo $result['id_note']?>">
             <button class="btn btn-primary mt-2 kanan" type="submit">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-upload kanan" viewBox="0 0 16 16">
   <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
